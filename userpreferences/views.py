3395f5bed3cab8e12291c4ef4e1b92a612316244
wleej <georgiawang5332@gmail.com>
2021-12-07 16:57:05 +0800

    chartJs fetch("/expenses/expense_category_summary") 資料要抓到

--- userpreferences/views.py
+++ userpreferences/views.py
@@ -11,7 +11,7 @@ def index(request):
     currency_data = []
     file_path = os.path.join(settings.BASE_DIR, 'currencies.json')
 
-    with open(file_path, 'r') as json_file:
+    with open(file_path, 'r', encoding="utf-8") as json_file:
         data = json.load(json_file)
         for k, v in data.items():
             currency_data.append({'name': k, 'value': v})
